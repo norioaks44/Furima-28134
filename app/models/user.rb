0b825ba58a48f3457ad35f775cf676c932b7d866
norioaks44 <jrc.katou@gmail.com>
2020-10-29 21:52:31 +0900

    ログイン、ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
     with_options presence: true do        
-      validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' }
-      validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' }
-      validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
-      validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+      validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, 
+        # message: '全角文字を使用してください' 
+      }
+      validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/,
+        # message: '全角文字を使用してください' 
+      }
+      validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/,
+        # message: "は全角カタカナで入力して下さい。"
+      }
+      validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/,
+        # message: "は全角カタカナで入力して下さい。"
+      }
     end
 end
