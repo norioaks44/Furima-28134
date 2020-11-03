a3b520c391b1aac264b01ed1c5f83b3b430c7761
norioaks44 <jrc.katou@gmail.com>
2020-11-03 20:26:36 +0900

    Update 20201027094549_devise_create_users.rb

--- db/migrate/20201027094549_devise_create_users.rb
+++ db/migrate/20201027094549_devise_create_users.rb
@@ -11,7 +11,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name,          null: false, default: ""
       t.string :first_name_kana,    null: false, default: ""
       t.string :last_name_kana,     null: false, default: ""
-      t.date   :birth_date,         null: false, default: ""
+      t.datetime   :birth_date,         null: false
+      # , default: ""
 
       ## Recoverable
       t.string   :reset_password_token
