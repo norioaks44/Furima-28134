b77aafc784afa76483ae7bc382d41937415f289d
norioaks44 <jrc.katou@gmail.com>
2020-11-03 23:06:07 +0900

    画像投稿機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :image, presence: true, unless: :was_attached?
   
+  def was_attached?
+    self.image.attached
+  end
 end
