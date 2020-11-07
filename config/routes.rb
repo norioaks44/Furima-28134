fb3ec2c97bf4f2a8de820ac329a78a4ecec28834
norioaks44 <jrc.katou@gmail.com>
2020-11-07 20:33:22 +0900

    商品詳細ページへ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   
-  resources :users 
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show]
   
 end
