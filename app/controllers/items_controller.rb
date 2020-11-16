b62728e2ac30803e8a2fadce41c36b3e7afe8c7e
norioaks44 <jrc.katou@gmail.com>
2020-11-16 21:10:51 +0900

    再購入防止の実装2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :set_celler_match, only: [:edit, :destroy]
-
+  before_action :after_purchase_block, only: [:edit, :update, :destroy]
+  
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -57,6 +58,12 @@ class ItemsController < ApplicationController
   def set_celler_match
     if current_user.id != @item.user_id
       render :show
+    end
+  end
+  
+  def after_purchase_block
+    if @item.purchase != nil
+      redirect_to root_path
     end
   end
 
